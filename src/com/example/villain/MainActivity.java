e67fd00516399b4a6630252eb18f51d7a27ccf60
g4-km <mazurekm4@gmail.com>
2014-03-18 18:15:39 -0700

    Creating Kris_march18 branch

--- src/com/example/villain/MainActivity.java
+++ src/com/example/villain/MainActivity.java
@@ -30,13 +30,13 @@ public class MainActivity extends Activity {
 		Log.i(playername, "after TextView edit (MainActivity)"+t1.isInEditMode()+" " + t2.isInEditMode());
 	}
 
-/*	@Override
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	*/
+	
 	 @Override
     protected void onResume() {
     	super.onResume();
